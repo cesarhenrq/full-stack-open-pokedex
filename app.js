a05b6034204a76b0475377c40c1f873370af9544
Cééesar Henrique <cesaarhvb@gmail.com>
2024-07-16 22:44:44 -0300

    chore: update /version endpoint to return app version 1.0.3 and add error handling to /health endpoint

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.get("/version", (req, res) => {
-  res.send("1.0.2");
+  res.send("1.0.3");
 });
 
 app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw "error...  ";
   res.send("ok");
 });
 
