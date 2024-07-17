453c5feb33b28fa2d565538d8175a2aa5b5bb65c
Cééesar Henrique <cesaarhvb@gmail.com>
2024-07-16 21:43:54 -0300

    feat: add /version endpoint to retrieve app version

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+app.get("/version", (req, res) => {
+  res.send("1.0.0");
+});
+
 app.listen(PORT, () => {
   console.log("server started on port 5000");
 });
