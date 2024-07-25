33048b2991510f60b642623670e07d5139565426
Cééesar Henrique <cesaarhvb@gmail.com>
2024-07-24 22:05:53 -0300

    chore: remove eslint-disable comments in app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-/* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 /* eslint-disable semi */
-/* eslint-disable quotes */
-/* eslint-disable no-undef */
 const express = require("express");
 const app = express();
 
